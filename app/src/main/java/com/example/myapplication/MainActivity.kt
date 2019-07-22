155f6781230878102ca733ad17c8573f48bb0eb6
deg <daniel.esobar@udea.edu.co>
2019-07-22 18:07:55 -0500

    segundos cambios prueba 7

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 //..................................................................
+    //ultimas pruebas
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
